63efd66a9b2c7b789d0ae0ba97b4c9843229ec94
Tarot Developer <dev@tarot-yorum.fun>
2025-12-12 09:41:21 +0300

    Fix: Syntax Error in TopicPoll Model (Missing Brace)

--- app/Models/TopicPoll.php
+++ app/Models/TopicPoll.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class TopicPoll extends Model
+{
     protected $fillable = ['target_date', 'options', 'winning_topic', 'is_processed'];
 
     protected $casts = [
         'options' => 'array',
         'target_date' => 'date'
     ];
+}
